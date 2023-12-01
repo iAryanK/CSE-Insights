e1e42c3af0095d46bb6ad515e086c4557b9fc9f0
Aryan <aryaninmail@gmail.com>
2023-12-01 19:57:59 +0530

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import Feed from "@components/Feed"
+
+const Home = () => {
+    return (
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">Discover and Share
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center">Deep CSE Insights</span>
+            </h1>
+            <p className="desc text-center">Unlock the Power of Computer Science & Engineering • CSE Insights - Your Gateway to Practical Tips, Proven Strategies, and College life.</p>
+        
+        {/* Feed  */}
+        <Feed/>
+        </section>
+    )
+}
+
+export default Home
